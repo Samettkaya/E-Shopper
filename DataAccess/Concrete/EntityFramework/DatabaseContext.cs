b3c6729540aa08971ad76ebd35af8a5d5813fdf8
Samettkaya <kaya67380@gmail.com>
2021-09-02 22:29:07 +0300

    E-Shopper Project

---
+++ DataAccess/Concrete/EntityFramework/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class DatabaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=E_Shopper;Trusted_Connection=true");
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brands> Brands { get; set; }
+        public DbSet<Addresses> Addresses { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Cities> Cities { get; set; }
+        public DbSet<Countries> Countries { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<OrderStatuses> OrderStatuses { get; set; }
+
+
+
+    }
+}
---
+++ Entities/Concrete/Categories.cs
@@ -0,0 +1,16 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Categories:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool Active { get; set; }
+    }
+}
